3ae2f788347ab0990d703fe6538352c86c1b47b0
AbdalrahmanAlzaro <abdalrahman.h.alzaro@gmail.com>
2023-10-17 14:24:48 +0300

    add file

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { footerVariants, staggerChildren } from "../../utils/motion";
+import css from "./Footer.module.scss";
+import { motion } from "framer-motion";
+const Footer = () => {
+  return (
+    <motion.section
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper}`}
+    >
+      <motion.div
+        variants={footerVariants}
+        className={`innerWidth yPaddings flexCenter ${css.container}`}
+      >
+        <div className={css.left}>
+          <span className="primaryText">
+            Let's make something <br />
+            amazing together.
+          </span>
+          <span className="primaryText">
+            Start by{" "}
+            <a href="mailto:abdalrahman.h.alzaro@gmail.com">saying hi</a>
+          </span>
+        </div>
+
+        <div className={css.right}>
+          <div className={css.info}>
+            <span className="secondaryText">Information</span>
+            <p> Zarqa, New Zarqa, JORDAN</p>
+          </div>
+          <ul className={css.menu}>
+            <li>
+              <a
+                href="#experties"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Services
+              </a>
+            </li>
+
+            <li>
+              {" "}
+              <a
+                href="#portfolio"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                {" "}
+                Works{" "}
+              </a>
+            </li>
+            {/* <li>Notes</li> */}
+            <li>
+              {" "}
+              <a
+                href="#work"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                {" "}
+                Experience
+              </a>
+            </li>
+          </ul>
+        </div>
+      </motion.div>
+    </motion.section>
+  );
+};
+
+export default Footer;
---
+++ src/utils/data.js
@@ -0,0 +1,156 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "Website Design",
+    projects: 16,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Mobile App Design",
+    projects: 3,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "Brand Identity",
+    projects: 7,
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+  "With a background in Full Stack Web Development and ongoing studies in Computer Information Systems, I can assist clients in creating innovative and user-friendly web solutions. My portfolio showcases my proficiency in web projects",
+  // "and I can offer expertise in building engaging websites, e-commerce platforms, and efficient MERN stack applications. I'm here to help clients bring their digital visions to life with professionalism and technical excellence.",
+];
+
+export const workExp = [
+    {
+    place: "New Man Services",
+    tenure: "Aug 2020 - Sep 2021",
+    role: "UI/UX Designer",
+    detail:
+      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
+  },
+  {
+    place: "Self-Employed",
+    tenure: "Aug 2021 - Sep 2022",
+    role: "Full Stack Wep Developer",
+    detail:
+      "I have practical experience gained during an intensive Full Stack Web Development course at Coding Academy by Orange, complemented by real-world project work. My expertise extends to building web applications, including a portfolio, e-commerce platform, and task/reservation management apps using the MERN stack.",
+  },
+  {
+    place: "Java Web Developer",
+    tenure: "Aug 202 - Sep 2023",
+    role: "specialize in creating web applications and websites using Java-based technologies",
+    detail:
+      "I have 1 years of experience as a Java Web Developer, creating and maintaining web applications using Java EE and Spring Framework.",
+  },
+];
+
+export const comments = [
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
